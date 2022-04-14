6556c6cce198b04a06e7acd91781045abe22bff3
wanglufei <2686221966@qq.com>
2022-04-14 20:23:41 +0800

    分页插件的配置

--- yeb-server/src/main/java/com/uin/server/config/MybatisPlugs/MybatisPlusConfig.java
+++ yeb-server/src/main/java/com/uin/server/config/MybatisPlugs/MybatisPlusConfig.java
@@ -8,13 +8,20 @@ import org.springframework.context.annotation.Configuration;
  * \* Created with IntelliJ IDEA.
  * \* @author wanglufei
  * \* Date: 2021年08月13日 10:50
- * \* Description: Mybatis 分页配置
+ * \* Description: Mybatis 分页插件配置
  * \
  */
 @Configuration
 public class MybatisPlusConfig {
+    /**
+     * @Bean 指示一个方法生成一个由 Spring 容器管理的 bean
+     *
+     * @return com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor
+     * @author wanglufei
+     * @date 2022/4/14 8:22 PM
+     */
     @Bean
-    public PaginationInterceptor paginationInterceptor(){
+    public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
     }
 
